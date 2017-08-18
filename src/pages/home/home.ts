d86cc01af73d2b541d6e4652cc607eff252910e4
wbl <651753175@qq.com>
2017-08-18 08:55:23 +0800

    在线更新

---
+++ src/pages/home/home.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { NavController, IonicPage, AlertController } from 'ionic-angular';
+import { ZBar, ZBarOptions } from '@ionic-native/zbar';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  constructor(public navCtrl: NavController, private zbar: ZBar, private barcodeScanner: BarcodeScanner, private transfer: FileTransfer, private file: File, private alertCtrl: AlertController) {
+  }
+  TestPage() {
+    this.navCtrl.push('TestPage')
+
+  }
+
+  download() {
+    this.alertCtrl.create({
+      title: '更新版本',
+      subTitle: '10% of battery remaining',
+      enableBackdropDismiss: false,
+      buttons: ['Dismiss']
+    }).present();
+    const fileTransfer: FileTransferObject = this.transfer.create();
+    const apk = this.file.dataDirectory  + 'coal.apk';
+
+   /*  fileTransfer.onProgress((event: ProgressEvent) => {
+      let num = Math.floor(event.loaded / event.total * 100);
+      if (num === 100) {
+
+      } else {
+        let title = document.getElementsByClassName('alert-title')[0];
+        title && (title.innerHTML = '下载进度：' + num + '%');
+      }
+    }); */
+    fileTransfer.download('https://coalline.com/files/apks/plant0801.apk', apk).then((entry) => {
+      /*  window.alert('download complete'+entry.toURL()); */
+      /*  window['install'].install(apk.replace('file://', '')); */
+      window['install'].install(apk.replace('file://', ''));
+    });
+
+    fileTransfer.onProgress((event:ProgressEvent)=>{
+      let num = Math.floor(event.loaded / event.total * 100);
+      if(num<=100){
+        let title = document.getElementsByClassName('alert-sub-title')[0];
+        title && (title.innerHTML = '下载进度：' + num + '%');
+      }
+    })
+  
+  }
+}
