e3eac0ecb348141c612bec66bb35102065be7cf7
wbl <651753175@qq.com>
2017-08-18 10:24:49 +0800

    物理返回键

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform,ToastController,IonicApp } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -11,12 +11,12 @@ import { ListPage } from '../pages/list/list';
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
-
+  backButtonPressed: boolean = false;  //用于判断返回键是否触发
   rootPage: any = HomePage;
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen,private toastCtrl:ToastController,private app:IonicApp) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -33,6 +33,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.registerBackButtonAction();//注册返回按键事件
     });
   }
 
@@ -41,4 +42,39 @@ export class MyApp {
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
   }
+
+  registerBackButtonAction() {
+    this.platform.registerBackButtonAction(() => {
+      //如果想点击返回按钮隐藏toast或loading或Overlay就把下面加上
+      /* this.app._toastPortal.getActive() || this.app._loadingPortal.getActive() || this.app._overlayPortal.getActive() */
+      let activePortal = this.app._modalPortal.getActive()||this.app._toastPortal.getActive() || this.app._loadingPortal.getActive() || this.app._overlayPortal.getActive();
+      if (activePortal) {
+        activePortal.dismiss().catch(() => {});
+        activePortal.onDidDismiss(() => {});
+        return;
+      }
+      let activeVC = this.nav.getActive();
+    /*   let tabs = activeVC.instance.tabs;
+      let activeNav = tabs.getSelected(); */
+     
+     /*  return activeNav.canGoBack() ? activeNav.pop() : this.showExit() */
+     return this.showExit()
+    }, 1);
+  }
+
+  //双击退出提示框
+  showExit() {
+    if (this.backButtonPressed) { //当触发标志为true时，即2秒内双击返回按键则退出APP
+      this.platform.exitApp();
+    } else {
+      this.toastCtrl.create({
+        message: '再按一次退出应用',
+        duration: 2000,
+        position: 'top'
+      }).present();
+      this.backButtonPressed = true;
+      setTimeout(() => this.backButtonPressed = false, 2000);//2秒内没有再次点击返回则将触发标志标记为false
+    }
+  }
+
 }
